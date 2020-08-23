30d9fb87bc1b569ddece3bdbcb9d64e3c2f9b704
박성현 <park2light@gmail.com>
2020-08-23 14:41:50 +0900

    로그인버튼 클릭시 아이디와 비번을 검사해서 로직을 작성

--- app/src/main/java/kr/co/tjoeun/loginlogictest_20200823/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/loginlogictest_20200823/MainActivity.kt
@@ -2,10 +2,35 @@ package kr.co.tjoeun.loginlogictest_20200823
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val emailEdt = emailEdt.text.toString();
+            val passwordEdt = passwordEdt.text.toString();
+
+            if(emailEdt=="admin@test.com" && passwordEdt=="qwer"){
+
+                Toast.makeText(this,"관리자로그인입니다",Toast.LENGTH_SHORT).show()
+
+            }else{
+
+                Toast.makeText(this,"로그인실패",Toast.LENGTH_SHORT).show()
+
+            }
+
+
+        }
     }
+
+
+
+
 }
